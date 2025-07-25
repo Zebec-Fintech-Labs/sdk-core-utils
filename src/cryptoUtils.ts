4ac0ce8c67f0b4f8121cc2aa772c1ef8c9b151bd
Ashish Sapkota <ashishspkt6566@gmail.com>
2025-07-25 12:13:57 +0545

    feat: add function to hash with SHA256 algorithm and return hash buffer as output

--- src/cryptoUtils.ts
+++ src/cryptoUtils.ts
@@ -6,3 +6,11 @@ export async function hashSHA256(input: string) {
 
 	return hashHex;
 }
+
+export async function hashSHA256ToBuffer(input: string) {
+	const utf8 = new TextEncoder().encode(input);
+	const hashArrayBuffer = await crypto.subtle.digest("SHA-256", utf8);
+	const hashBuffer = Buffer.from(new Uint8Array(hashArrayBuffer));
+
+	return hashBuffer;
+}
